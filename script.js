c386ccd21f30f69f049eca742f3cdd904b8b1d17
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-06-20 17:21:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+ window.onload = function() {
+            var browserInfo = document.getElementById("browser-info");
+            
+            var browserName = navigator.userAgent;
+            var version = parseFloat(navigator.appVersion);
+            
+            browserInfo.textContent = "You are using " + browserName + " version " + version;
+        };
